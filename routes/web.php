7b3bee74e4d216ce07eda85728e4ad9344a41c3f
Abdullah Al Maruf <almaruf@softograph.com>
2025-06-30 10:21:18 +0600

    feat: Add clear-cache route and update autoload classmap for new API controllers and models

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('optimize');
+    return 'Cache cleared, optimized, and permissions set!';
+});
